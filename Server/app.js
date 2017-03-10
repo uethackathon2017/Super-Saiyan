ade0e1788ebe208bd64a18ba3304caffb95e9299
bqcuong <bqcuong2212@gmail.com>
2017-03-11 01:04:45 +0700

    add google api

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,7 @@ const EVENT_SIGN_INTERRUPT = 'sigint'.toUpperCase();
 let bodyParse = require('body-parser');
 let express = require('express');
 let http = require('http');
+require('dotenv').config();
 
 let app = express();
 let server = http.createServer(app);
@@ -28,5 +29,6 @@ function startApplication() {
         console.log('Server was started.');
 
         app.use(require('./routes/topic'));
+        app.use(require('./routes/map'));
     });
 }
