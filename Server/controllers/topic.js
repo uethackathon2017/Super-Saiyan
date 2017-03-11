4579b1755ea4f72f53e0c4bdf2143dbe12081e34
bqcuong <bqcuong2212@gmail.com>
2017-03-11 19:34:49 +0700

    add common words db

--- Server/controllers/topic.js
+++ Server/controllers/topic.js
@@ -6,4 +6,9 @@ function getTopics(req, res) {
     res.sendFile(path.dirname(__dirname) + "/data/topic_db.json");
 }
 
+function getCommonWords(req, res) {
+    res.sendFile(path.dirname(__dirname) + "/data/common_words_db.json");
+}
+
 module.exports.getTopics = getTopics;
+module.exports.getCommonWords = getCommonWords;
