ebf0a25c559992fb8c281904120322ecffe90fe8
ThanhNam <thanhnamitit@gmail.com>
2017-03-12 10:35:55 +0700

    nam commit

--- Android/app/src/main/java/com/supersaiyan/englock/activity/ChatRoomActivity.java
+++ Android/app/src/main/java/com/supersaiyan/englock/activity/ChatRoomActivity.java
@@ -4,6 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 
@@ -14,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.supersaiyan.englock.R;
 import com.supersaiyan.englock.databinding.ActivityChatRoomBinding;
 import com.supersaiyan.englock.model.Message;
+import com.supersaiyan.englock.view.adapter.MessageAdapter;
 
 /**
  * Created by thanh_000 on 3/11/2017.
@@ -23,6 +26,9 @@ public class ChatRoomActivity extends AppCompatActivity {
     private static final String TAG = "ChatRoomActivity";
     private ActivityChatRoomBinding binding;
     private String roomName;
+    private MessageAdapter adapter;
+    private LinearLayoutManager layoutManager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,12 +38,24 @@ public class ChatRoomActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         roomName = getIntent().getStringExtra("data");
         setUpListener();
+        setUpRecyclerView();
 
+    }
+
+    private void setUpRecyclerView() {
+        recyclerView = binding.recyclerMessageChat;
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter
+                = new MessageAdapter();
+        recyclerView.setAdapter(adapter);
     }
 
     public void setUpListener() {
         FirebaseDatabase.getInstance().getReference().child("room").child(roomName).addChildEventListener(listener);
-
     }
 
 
@@ -47,15 +65,20 @@ public class ChatRoomActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    public void sendMsg() {
-        FirebaseDatabase.getInstance().getReference().child("room").child(roomName).push().setValue(new Message("he", "he"));
+    public void sendClick() {
+        if (binding.edtMessage.getText().toString().trim().equals("")) return;
+        FirebaseDatabase.getInstance().getReference().child("room").child(roomName).push().setValue(new Message("Name", binding.edtMessage.getText().toString().trim()));
+        binding.edtMessage.setText("");
     }
 
 
     private ChildEventListener listener = new ChildEventListener() {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-            Log.i(TAG, dataSnapshot.toString());
+            Message message = dataSnapshot.getValue(Message.class);
+            adapter.addMessage(message);
+            layoutManager.smoothScrollToPosition(recyclerView, null,
+                    adapter.getItemCount() - 1);
         }
 
         @Override
